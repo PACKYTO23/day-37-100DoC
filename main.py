9d76b8ca9b1d852940bb7dace882d56c37f5e439
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2024-04-09 19:57:46 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+
+USERNAME = "franciscogp"
+TOKEN = "1234qwertyuiop56789"
+GRAPH_ID = "graph1"
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+
+# print(response.text)
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Writing Graph",
+    "unit": "Characters",
+    "type": "int",
+    "color": "momiji",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_params, headers=headers)
+
+# print(response.text)
+
+POST_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+post_params = {
+    "date": "20240409",
+    "quantity": "500",
+}
+
+response = requests.post(url=POST_ENDPOINT, json=post_params, headers=headers)
+
+print(response.text)
+
+website = "https://pixe.la/v1/users/franciscogp/graphs/graph1.html"
